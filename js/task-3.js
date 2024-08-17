f2deb7d0cc25f6ecf7d487f4d16134a8d879b999
vovaVat <vovavat97@gmail.com>
2024-08-17 15:57:49 +0300

    task2 and task3

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value){
+    const numb_list=[];
+    for (const num of numbers){
+        if(num > value){
+            numb_list.push(num);
+        }
+    }
+    return numb_list;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
